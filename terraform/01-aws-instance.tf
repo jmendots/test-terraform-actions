1fa4e93eb4feb3ff692f582eef647d7ff1b8a83d
Jose Manuel Mendoza Rubio <josem.mendorubio@gmail.com>
2022-03-13 14:01:19 +0100

    cicd

--- terraform/01-aws-instance.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "example" {
-  ami = "ami-06ad2ef8cd7012912"
-  instance_type = "t2.micro"
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,8 @@ terraform {
 provider "aws" {
   region = "eu-west-3"
 }
+
+resource "aws_instance" "example" {
+  ami = "ami-06ad2ef8cd7012912"
+  instance_type = "t2.micro"
+}
