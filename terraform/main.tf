3943489b3a56a9cfc539d34ffd673682dbff44e3
Jose Manuel Mendoza Rubio <josem.mendorubio@gmail.com>
2022-03-13 18:03:26 +0100

    cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "jmmendoza-tfstate"
-    key            = "terraform-deploy-example/00-aws-instance/terraform.tfstate"
+    key            = "test-terraform-actions/aws-instance/terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
@@ -17,6 +17,32 @@ terraform {
 
 provider "aws" {
   region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "jmmendoza-tfstate" # REPLACE WITH YOUR BUCKET NAME
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 resource "aws_instance" "example" {
