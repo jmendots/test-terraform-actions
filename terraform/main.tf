3592cc98adfa3d3c8b22755f81922020a8fa30f8
Jose Manuel Mendoza Rubio <josem.mendorubio@gmail.com>
2022-03-13 18:04:46 +0100

    delete ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-06ad2ef8cd7012912"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "example" {
+  # ami           = "ami-06ad2ef8cd7012912"
+  # instance_type = "t2.micro"
+# }
