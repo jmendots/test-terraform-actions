1c4eb692609a6c088c9b0f314bb59bb3606c6c20
Jose Manuel Mendoza Rubio <josem.mendorubio@gmail.com>
2022-03-13 18:05:56 +0100

    apply tf fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,6 +46,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 # resource "aws_instance" "example" {
-  # ami           = "ami-06ad2ef8cd7012912"
-  # instance_type = "t2.micro"
+# ami           = "ami-06ad2ef8cd7012912"
+# instance_type = "t2.micro"
 # }
